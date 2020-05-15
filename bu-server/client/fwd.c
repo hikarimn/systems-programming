20f0756cd36a1b124990eaa266fca69c0fd2a372
Hikari <37006149+hikarimn@users.noreply.github.com>
2020-05-15 10:25:52 -0500

    Update fwd.c

--- bu-server/client/fwd.c
+++ bu-server/client/fwd.c
@@ -92,50 +92,55 @@ int main(int argc, char **argv){
         fprintf(logFile, "received string: %s\n", received_buf);
         
 
-        char str1[] = "0";
-        // When the server does not have a copy of the file
-        if (strcmp(str1, received_buf) == 0){
-          strcpy(status_buf, "upload ");
-          strcat(status_buf, argv[1]);
-          strcat(status_buf, "/");
-          strcat(status_buf,entp->d_name);
-          strcat(status_buf, " ");
-          strcat(status_buf, s.st_size);
+        // char str1[] = "0";
+        // // When the server does not have a copy of the file
+        // if (strcmp(str1, received_buf) == 0){
+        //   strcpy(status_buf, "upload ");
+        //   strcat(status_buf, argv[1]);
+        //   strcat(status_buf, "/");
+        //   strcat(status_buf,entp->d_name);
+        //   strcat(status_buf, " ");
+        //   strcat(status_buf, s.st_size);
 
           // Sending the first part of updates
-          Rio_writen(clientfd, status_buf, strlen(status_buf));
+          // Rio_writen(clientfd, status_buf, strlen(status_buf));
           
           // Reading the stuff in the file and sending the second part of updates
-          char *buffer;
-          FILE *fh = fopen(path, "rb");
-          if ( fh != NULL ){
-            fseek(fh, 0L, SEEK_END);
-            long ftell = ftell(fh);
-            rewind(fh);
-            buffer = malloc(ftell);
-            if ( buffer != NULL ){
-              fread(buffer, ftell, 1, fh);
-              fclose(fh); 
-              fh = NULL;
+          // char *buffer;
+          // FILE *fh = fopen(path, "rb");
+          // if ( fh != NULL ){
+          //   fseek(fh, 0L, SEEK_END);
+          //   long ftell = ftell(fh);
+          //   rewind(fh);
+          //   buffer = malloc(ftell);
+          //   if ( buffer != NULL ){
+          //     fread(buffer, ftell, 1, fh);
+          //     fclose(fh); 
+          //     fh = NULL;
  
-              Rio_writen(clientfd, buffer, strlen(buffer));
-              free(buffer);
-            }
-            if (fh != NULL) fclose(fh);
-          }
+          //     Rio_writen(clientfd, buffer, strlen(buffer));
+          //     free(buffer);
+          //   }
+          //   if (fh != NULL) fclose(fh);
+          // }
 
-          Rio_readlineb(&rio, received_buf, MAXLINE);
-          fprintf(logFile, "received string: %s\n", received_buf);
+          // Rio_readlineb(&rio, received_buf, MAXLINE);
+          // fprintf(logFile, "received string: %s\n", received_buf);
+           close(clientfd);
         }
-        close(clientfd);
+       
       }
-    }
     closedir(watchDIR);
     /* Note the time and go to sleep. */
     time(&timer);
     sleep(60);
-  }
+
+    }
+
+    
+  
   
   fclose(logFile);
   return 0;
+
 }
