280a424b3a82a8b924cb7d84a9912004c7ff9788
Hikari <37006149+hikarimn@users.noreply.github.com>
2020-05-16 17:07:39 -0500

    added files

--- bu-server/client/fwd.c
+++ bu-server/client/fwd.c
@@ -8,40 +8,78 @@
 #include <dirent.h>
 #include "csapp.h"
 
-// - fw program (no changes)
-//      ./fw start test 
-// - fwd program  
-//     Scans the watched directory.
-//     For each file, send a (status) request to bu server to see 
-//     if the server has a copy of that file
-//     If the server doesn't have a backup file or the backup
-//     file is old, fwd will upload a copy of the file to 
-//     the server  using a (upload) request.
-//     fwd checks the change every 5 minutes.
-// - bu.c (new server application)
-//     Listen for connection on port 8000.
-//     Clients connect to the server and send a single request.
-//     Server sends back a response to  the request, 
-//     close the connection to the client,
-//     and go back to listening for another client connection.%    
-
 int run;
 
 void handleTERM(int x) {
   run = 0;
 }
 
-int main(int argc, char **argv){
-  FILE* logFile;
+void upload_file(char* dir,char* fileName,int filesize) {
+    int srcfd, clientfd;
+    char *host, *port, *cmd, *srcp;
+    char buffer[256];
+    rio_t rio;
+
+    host = "127.0.0.1";
+    port = "8000";
+    cmd = "upload\n";
+    clientfd = Open_clientfd(host, port);
+    Rio_readinitb(&rio, clientfd);
+
+    Rio_writen(clientfd, cmd, strlen(cmd));
+    strcpy(buffer,fileName);
+    strcat(buffer,"\n");
+    Rio_writen(clientfd, buffer, strlen(buffer));
+    sprintf(buffer,"%d\n",filesize);
+    Rio_writen(clientfd, buffer, strlen(buffer));
+
+	strcpy(buffer,dir);
+    strcat(buffer,"/");
+    strcat(buffer,fileName);
+    srcfd = Open(buffer, O_RDONLY, 0);
+    srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+    Close(srcfd); 
+    Rio_writen(clientfd, srcp, filesize);
+    Munmap(srcp, filesize);          
+
+	Rio_readlineb(&rio, buffer, 256);
+	Close(clientfd); 
+}
+
+void check_file(char* dir,char* fileName,int time,int size) {
+    int clientfd;
+    char *host, *port, *cmd, *srcp;
+    char buffer[256];
+    rio_t rio;
+    int serverTime;
+
+    host = "127.0.0.1";
+    port = "8000";
+    cmd = "status\n";
+    clientfd = Open_clientfd(host, port);
+    Rio_readinitb(&rio, clientfd);
+
+    Rio_writen(clientfd, cmd, strlen(cmd));
+    strcpy(buffer,fileName);
+    strcat(buffer,"\n");
+    Rio_writen(clientfd, buffer, strlen(buffer));
+    
+	Rio_readlineb(&rio, buffer, 256);
+	Close(clientfd);
+    sscanf(buffer,"%d",&serverTime);
+    if(serverTime < time)
+      upload_file(dir,fileName,size);
+}  
+
+int main(int argc, char **argv)
+{
   struct sigaction action, old_action;
   time_t timer;
+  int buModTime;
   DIR* watchDIR;
   struct dirent* entp;
   struct stat s;
   char path[1024];
-  char *host, *port, buf[MAXLINE], status_buf[MAXLINE], received_buf[MAXLINE];
-  int clientfd;
-  rio_t rio;
 
   /* Install the handler for SIGTERM */
   action.sa_handler = handleTERM;
@@ -49,154 +87,41 @@ int main(int argc, char **argv){
   action.sa_flags = 0;
   sigaction(SIGTERM, &action, &old_action);
 
-  /* Open the log file and start watching for changes. **/
-  run = 1;
-  logFile = fopen("fwd.log","a");
-  host = "127.0.0.1";
-  port = "8000";
-  // clientfd = Open_clientfd(host, port);
-  // Rio_readinitb(&rio, clientfd);
+  /* Run the initial scan. */
+  watchDIR = opendir(argv[1]);
+  while((entp = readdir(watchDIR))!=NULL) {
+    strcpy(path,argv[1]);
+	strcat(path,"/");
+	strcat(path,entp->d_name);
+	stat(path,&s);
+	/* Only pay attention to files, and ignore directories. */
+	if(S_ISREG(s.st_mode)) 
+	  check_file(argv[1],entp->d_name,s.st_mtime,s.st_size);
+  }
+  closedir(watchDIR);
 
-  /** You had
-  while(1){
-  I fixed this. **/
+  /* Start watching for changes. **/
+  run = 1;
   while(run == 1) {
+    /* Note the time and go to sleep. */
+    time(&timer);
+    sleep(60);
     /* Scan the watched directory for changes. */
     watchDIR = opendir(argv[1]);
     while((entp = readdir(watchDIR))!=NULL) {
       strcpy(path,argv[1]);
       strcat(path,"/");
       strcat(path,entp->d_name);
-	  /** You had
-      strcat(path,"\n");
-      This puts a \n at the end of the path. You did this because 
-      you were getting ready to send the file name to the server later.
-      This is wrong for two reasons. The first reason is that you are going
-      to use this path string in the stat call below. I suspected that having
-      the \n at the end of the path would cause the stat call to fail. To
-      test this suspicion I put some logic around the stat call to test for
-      errors. Sure enough, when the stat call ran it always failed. The 
-      second reason this is wrong is that you don't want to send the full path
-      to the server. The only thing the server needs is the file name, and you
-      can get that from just entp->d_name. **/
-      strcpy(status_buf, "status\n");
-
-      fprintf(logFile,"%s * \n", status_buf);
-      
-      
-      if(stat(path,&s) != 0)
-        printf("Error in stat call.\n");
+      stat(path,&s);
       /* Only pay attention to files, and ignore directories. */
       if(S_ISREG(s.st_mode)) {
-        
-        /** You had
-        fprintf(logFile,"Checking if %s exists. \n", status_buf);
-        I fixed this. **/
-        fprintf(logFile,"Checking if %s exists. \n", entp->d_name);
-
-        clientfd = Open_clientfd(host, port);
-        Rio_readinitb(&rio, clientfd);
-
-        Rio_writen(clientfd, status_buf, strlen(status_buf));
-        /** I added this: **/ strcpy(status_buf,entp->d_name); strcat(status_buf,"\n");
-        Rio_writen(clientfd, status_buf, strlen(status_buf));
-        // close(clientfd);
-        
-       /* You had
-
-        // size_t n; 
-
-        // This part is not working 
-        // size_t n; 
-        // while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:eof
-        //   Rio_readlineb(&rio, received_buf, MAXLINE);
-         //  fprintf(logFile, "received string: %s\n", received_buf);
-        // }
-        
-        Now that the server is fixed, we can go ahead and do the read. **/
-        Rio_readlineb(&rio, received_buf, MAXLINE);
-        /** Also, the server is designed to close the connection to the client after 
-            each request is complete. We should now close our connection to the server. **/
-        close(clientfd);
-        
-        /** You had
-        char str1[] = "0";
-        Note that even when the server sends you a 0 there will also be a \n at the end of the line. **/
-        char* str1 = "0\n";
-        // When the server does not have a copy of the file
-		/** You need an additional case in your if statement. If the modification time 
-            of the remote file is less than the modification time of the local file you 
-            should upload as well. **/
-        if (strcmp(str1, received_buf) == 0){
-          /** Each separate request to the server requires a new connection. **/
-          clientfd = Open_clientfd(host, port);
-          strcpy(status_buf, "upload\n");
-          // Sending the "upload" req
-          Rio_writen(clientfd, status_buf, strlen(status_buf));
-
-          /** You had
-          strcpy(status_buf, argv[1]);
-          strcat(status_buf, "/");
-          strcat(status_buf,entp->d_name);
-          strcat(status_buf, "\n");
-          For an upload request you should only send the file name, not the full path. **/
-          strcpy(status_buf, entp->d_name);
-          strcat(status_buf, "\n");
-          
-          // Sending the file name
-          Rio_writen(clientfd, status_buf, strlen(status_buf));
-           
-          /** You had
-          strcpy(status_buf, s.st_size);
-          This won't work, because st_size is an integer, not a string. I fixed this. **/
-          sprintf(status_buf,"%ld\n",s.st_size);
-          // Sending the file size
-          Rio_writen(clientfd, status_buf, strlen(status_buf));
-
-          // Reading the stuff in the file and sending the second part of updates
-          char *contents_buff;
-          
-          FILE *fh = fopen(path, "rb");
-          if ( fh != NULL ){
-            fseek(fh, 0L, SEEK_END);
-            long ftell_long = ftell(fh);
-            rewind(fh);
-            contents_buff = malloc(ftell_long);
-            if ( contents_buff != NULL ){
-              /** You had
-              fread(contents_buff, ftell_long, 1, fh);
-              I fixed this. **/
-              fread(contents_buff, 1, ftell_long, fh);
-              fclose(fh); 
-              fh = NULL;
-              // Sending the file contents
-              /** You had
-              Rio_writen(clientfd, contents_buff, strlen(contents_buff));
-              When you read the file contents using fread, there will not be a 
-              terminating \0 at the end of the buffer. This means that you can't use strlen here.
-              I fixed this for you. **/
-              Rio_writen(clientfd, contents_buff, (int) ftell_long);
-              free(contents_buff);
-            }
-            if (fh != NULL) fclose(fh);
-          } else
-            printf("Could not open %s\n",path);
-
-            Rio_readlineb(&rio, received_buf, MAXLINE);
-            fprintf(logFile, "received string: %s\n", received_buf);
-            close(clientfd);
-          }
-       
+        /* Upload recently modified files. */
+        if(difftime(s.st_mtime,timer) > 0) {
+          upload_file(argv[1],entp->d_name,s.st_size);
         }
+      }
     }
     closedir(watchDIR);
-    /* Note the time and go to sleep. */
-    time(&timer);
-    sleep(60);
-
-    } 
-  
-  fclose(logFile);
+  }
   return 0;
-
 }
--- bu-server/server/bu.c
+++ bu-server/server/bu.c
@@ -1,143 +1,62 @@
-
 #include "csapp.h"
 
-
-
-/** You originally had this:
-char* GetFileModificationTime(char *path) {
-    struct stat attr;
-    char time_buf[128]; 
-    if(stat(path,&attr) != 0)
-        printf("Error in stat call.\n");
-    strcpy(time_buf, ctime(&attr.st_mtime));
-    strncat(time_buf, "\n", 128);
-    // printf("Last modified time: %s", ctime(&attr.st_mtime));
-    return time_buf;
-}
-The problem here is that you are returning a pointer to a local array.
-That local array will vanish as soon as you exit the function.
-I fixed this for you: **/
-void GetFileModificationTime(char *path,char* size_buffer) {
-    struct stat attr;
-    stat(path,&attr);
-    sprintf(size_buffer,"%ld\n",attr.st_mtime);
-    printf("Last modified time: %ld\n", attr.st_mtime);
-}
-
-
-int FileExists (char *filename) {
-  struct stat buffer;   
-  if (stat (filename, &buffer) == 0) return 1;
-  return 0;
-}
-
-
-void communicate(int connfd) {
-    size_t n; 
-    char buf[MAXLINE], path_buf[MAXLINE], size_buf[MAXLINE]; 
-    rio_t rio;
-    char* word;
-    FILE *fp;
-    
-    Rio_readinitb(&rio, connfd);
-    // while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:eof
-	    n = Rio_readlineb(&rio, buf, MAXLINE);
-        printf("received string: %s\n", buf);
-        printf("server received %d bytes\n", (int)n);
-
-        // When it is a status call 
-        if(strcmp(buf, "status\n") == 0){
-            strcpy(path_buf, "copy/");
-            Rio_readlineb(&rio, buf, MAXLINE); // read the file name
-            /** You had
-            strncat(path_buf, buf, MAXLINE);
-            This will cause a problem similar to the problem you had in fwd.c, because
-            buf has a \n at the end of it. I fixed this for you. **/
-            buf[strlen(buf)-1] = '\0'; // Replace \n with \0
-            strcat(path_buf,buf);
-            printf("status!: %s\n", path_buf);
-
-            // when the file exists
-            if (FileExists(path_buf)){
-                /** You had
-                Rio_writen(connfd, GetFileModificationTime(path_buf), MAXLINE);
-                printf("%s exists.\n", path_buf);
-                printf("Modified at %s\n", GetFileModificationTime(path_buf));
-                I fixed this to match the changes I made to GetFileModificationTime. **/
-                GetFileModificationTime(path_buf,size_buf);
-                Rio_writen(connfd,size_buf,strlen(size_buf));
-            } else {
-                /** You had
-                Rio_writen(connfd, "0\n", MAXLINE);
-                I fixed this. **/
-                Rio_writen(connfd, "0\n", 2);
-                printf("The file does not exist\n");
-            }
-
-        } else if(strcmp(buf, "upload\n") == 0){
-            Rio_readlineb(&rio, buf, MAXLINE); // read the file name
-            strcpy(path_buf, "copy/");
-            /** You had
-            strncat(path_buf, buf, MAXLINE);
-            buf has a newline character at the end of it. We want to not include that
-            in the path buffer. **/
-            strncat(path_buf, buf, strlen(buf)-1);
-            Rio_readlineb(&rio, buf, MAXLINE); // read the file size
-            strcpy(size_buf, buf);
-            /** Once again, the buffer you received from the client has a \n at the end of it.
-                We need to get rid of it: **/ size_buf[strlen(size_buf)-1] = '\0';
-            int size = 0;
-            /** You had
-        	size = *(unsigned int*)(size_buf);
-            You can't convert a string to an int by doing a type cast. I fixed this for you. **/
-            size = atoi(size_buf);
-            /** There is no guarantee that the file the client sent you will fit in buf.
-            What you have to do instead is to malloc a buffer large enough to receive the file.
-            Also, you need to use Rio_readn here instead of Rio_readlineb. 
-            Rio_readlineb(&rio, buf, MAXLINE); // read the file contents
-            I fixed this: **/
-            char* file_buf = malloc(size);
-            Rio_readn(connfd,file_buf,size);
-            /** You had
-            fp = fopen(path_buf, "w");
-            fseek(fp, size , SEEK_SET); 
-            fprintf(fp,  "%s", buf); 
-            fclose(fp);
-            This will fail because the text you received does not have a terminating \0 at
-            the end of it. I fixed this for you. **/
-            printf("Path is %s\n",path_buf);
-            int fd = Open(path_buf,O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
-            Write(fd,file_buf,size);
-            Close(fd);
-            free(file_buf);
-            /** You had
-            Rio_writen(connfd, "stored\n", n);
-            I fixed this. **/
-            char* msg = "stored\n";
-            Rio_writen(connfd, "stored\n", strlen(msg));
-        } 
-}
-
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
-    struct sockaddr_storage clientaddr;  /* Enough space for any address */  //line:netp:echoserveri:sockaddrstorage
-    char client_hostname[MAXLINE], client_port[MAXLINE];
-
-    if (argc != 2) {
-	fprintf(stderr, "usage: %s <port>\n", argv[0]);
-	exit(0);
-    }
+    struct sockaddr_storage clientaddr;
 
     listenfd = Open_listenfd(argv[1]);
     while (1) {
-    	clientlen = sizeof(struct sockaddr_storage); 
-	    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-        Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE, 
-                        client_port, MAXLINE, 0);
-        printf("Connected to (%s, %s)\n", client_hostname, client_port);
-	    communicate(connfd);
-	    Close(connfd);
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
+        Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE,
+                    port, MAXLINE, 0);
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
+        serve_request(connfd);  
+        Close(connfd); 
     }
-    exit(0);
 }
+
+void serve_request(int connfd)
+{
+    char buffer[MAXLINE], fileName[MAXLINE];
+    char *fileBuf;
+    int size;
+    rio_t rio;
+    int fd;
+    struct stat s;
+
+    /* Read request */
+    Rio_readinitb(&rio, connfd);
+    if (!Rio_readlineb(&rio, buffer, MAXLINE))
+        return;
+    if (strcmp(buffer, "upload\n") == 0) {
+        Rio_readlineb(&rio, fileName, MAXLINE);
+        Rio_readlineb(&rio, buffer, MAXLINE);
+        printf("Request to upload %s\n",fileName);
+        sscanf(buffer,"%d",&size);
+        fileBuf = Malloc(size);
+        if(fileBuf != NULL) {
+           Rio_readnb(&rio,fileBuf,size);
+           fd = Open(fileName,O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
+           Write(fd,fileBuf,size);
+           Close(fd);
+           Free(fileBuf);
+        }
+        strcpy(buffer,"stored\n");
+        Rio_writen(connfd,buffer,strlen(buffer));
+    } else if(strcmp(buffer,"status\n") == 0) {
+        Rio_readlineb(&rio, fileName, MAXLINE);
+        printf("Request to stat %s\n",fileName);
+        fileName[strlen(fileName)-1] = '\0';
+        if(stat(fileName,&s) == 0)
+          sprintf(buffer,"%d\n",s.st_mtime);
+        else
+          strcpy(buffer,"0\n");
+        Rio_writen(connfd,buffer,strlen(buffer));
+    }
+        
+}
+
