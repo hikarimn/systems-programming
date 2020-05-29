4a0df0c38fc9d07d3aacaf7cd0d185aabbe406ad
Hikari <37006149+hikarimn@users.noreply.github.com>
2020-05-28 21:32:36 -0500

    added files for concurrency

---
+++ threads/client/fwd.c
@@ -0,0 +1,355 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <unistd.h>
+#include <dirent.h>
+#include "csapp.h"
+
+int run;
+/* Locations of important files */
+const char* pidFileLoc = "/run/fwd.pid";
+const char* confFileLoc = "/etc/fwd.conf";
+const char* logFileLoc = "/var/log/fwd.log";
+FILE *logFile;
+
+static sem_t mutex;
+/* Global SIGHUP received flag */
+static volatile sig_atomic_t hupReceived = 0;
+/* Global pointer for a linked list */
+struct node* currentNode;
+
+void handleTERM(int x) {
+  run = 0;
+}
+
+static void init_echo_cnt(void){
+        Sem_init(&mutex, 0, 1);
+        // byte_cnt = 0;
+}
+
+struct node {
+  char* text;
+  struct node *next;
+};
+
+/* Make a new node to hold a copy of txt. */
+struct node *make_node(char* txt) {
+  struct node *new_node;
+  new_node = (struct node*) malloc(sizeof(struct node));
+  new_node->text = (char*) malloc(strlen(txt));
+  strcpy(new_node->text,txt);
+  new_node->next = NULL;
+  return new_node;
+}
+
+/* Attach a new node to the end of a chain of nodes. */
+void append_node(struct node *chain,char* txt) {
+  struct node *current = chain;
+  while(current->next != NULL)
+    current = current->next;
+  current->next = make_node(txt);
+}
+
+/*
+ * becomeDaemon - convert this process to a daemon
+ */
+int becomeDaemon(char *home)
+{
+  int fd;
+  pid_t pid;
+  FILE* pidFile;
+
+  if((pid = Fork()) != 0) { /* Become background process */
+    exit(0);  /* Original parent terminates */
+  }
+
+  if(setsid() == -1) /* Become leader of new session */
+    return -1;
+
+  if((pid = Fork()) != 0) { /* Ensure we are not session leader */
+	/** Prepare pid file and terminate **/
+	pidFile = fopen(pidFileLoc,"w");
+	fprintf(pidFile,"%d",pid);
+	fclose(pidFile);
+    exit(0);
+  }
+
+  chdir(home); /* Change to home directory */
+
+  Close(STDIN_FILENO); /* Reopen standard fd's to /dev/null */
+
+  fd = Open("/dev/null", O_RDWR, 0);
+
+  if (fd != STDIN_FILENO)         /* 'fd' should be 0 */
+    return -1;
+  if (Dup2(STDIN_FILENO, STDOUT_FILENO) != STDOUT_FILENO)
+    return -1;
+  if (Dup2(STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO)
+    return -1;
+
+  return 0;
+}
+
+
+void upload_file(char* dir,char* fileName,int filesize) {
+    int srcfd, clientfd;
+    char *host, *port, *cmd, *srcp;
+    char buffer[256];
+    rio_t rio;
+
+    host = "127.0.0.1";
+    port = "8000";
+    cmd = "upload\n";
+    clientfd = Open_clientfd(host, port);
+    Rio_readinitb(&rio, clientfd);
+
+    Rio_writen(clientfd, cmd, strlen(cmd));
+    strcpy(buffer,fileName);
+    strcat(buffer,"\n");
+    Rio_writen(clientfd, buffer, strlen(buffer));
+    sprintf(buffer,"%d\n",filesize);
+    Rio_writen(clientfd, buffer, strlen(buffer));
+
+	strcpy(buffer,dir);
+    strcat(buffer,"/");
+    strcat(buffer,fileName);
+    srcfd = Open(buffer, O_RDONLY, 0);
+    srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+    Close(srcfd); 
+    Rio_writen(clientfd, srcp, filesize);
+    Munmap(srcp, filesize);          
+
+	Rio_readlineb(&rio, buffer, 256);
+	Close(clientfd); 
+}
+
+void check_file(char* dir,char* fileName,int time,int size) {
+    int clientfd;
+    char *host, *port, *cmd, *srcp;
+    char buffer[256];
+    rio_t rio;
+    int serverTime;
+
+    host = "127.0.0.1";
+    port = "8000";
+    cmd = "status\n";
+    clientfd = Open_clientfd(host, port);
+    Rio_readinitb(&rio, clientfd);
+
+    Rio_writen(clientfd, cmd, strlen(cmd));
+    strcpy(buffer,fileName);
+    strcat(buffer,"\n");
+    Rio_writen(clientfd, buffer, strlen(buffer));
+    
+	Rio_readlineb(&rio, buffer, 256);
+	Close(clientfd);
+    sscanf(buffer,"%d",&serverTime);
+    if(serverTime < time)
+      upload_file(dir,fileName,size);
+}  
+
+
+
+/*
+readConfig - read the configuration file
+A line for each directory fwd has to watch. 
+On that line will be the path to the directory in question, a space, and a name to use for the folder on the bu server.
+A blank line at the end end of the list of directories.
+The address of the bu server.
+The port number of the bu server.
+ */
+/*
+ * readConfig - read the configuration file
+ */
+int *readConfig(char* address, char* port,struct node* *ptr) {
+  /* Code to open config file goes here. */
+  FILE *config;
+  config = fopen(confFileLoc,"r");
+  if(config == NULL)
+    printf("unable to read the config file");
+    return 1;
+
+  char buffer[256];
+  char folderNameBuf[256];
+  /* Code to read a path and a folder name into buffer goes here. */
+  fscanf(config, "%s %s", buffer, folderNameBuf);
+  strcat(buffer, " ");
+  strcat(buffer, folderNameBuf);
+  *ptr = make_node(buffer);
+  while(1) {
+    /* Code to read a path into buffer goes here. */
+    fscanf(config, "%s %s", buffer, folderNameBuf);
+    strcat(buffer, " ");
+    strcat(buffer, folderNameBuf);
+    if(strlen(buffer) == 0) /* Stop when you hit a blank line. */
+       break;
+    append_node(*ptr,buffer);
+  }
+
+  /* Code to read host and port goes here. */
+  fscanf(config,"%s",address);
+  fscanf(config,"%s",port);
+  fclose(config);
+  return 0;
+}
+
+int readConfigBefore(char *path, char *address,char *port) {
+  FILE *config;
+
+  config = fopen(confFileLoc,"r");
+  if(config == NULL)
+    return -1;
+  fscanf(config,"%s",path);
+  fscanf(config,"%s",address);
+  fscanf(config,"%s",port);
+  fclose(config);
+  return 0;
+}
+
+/*
+ * logOpen - open the log file
+ */
+int logOpen() {
+  logFile = fopen(logFileLoc,"a");
+  if(logFile == NULL)
+    return -1;
+  return 0;
+}
+
+/* 
+ * logMessage - write a message to the log file
+ */
+void logMessage(char *msg) {
+  P(&mutex);
+  if(logFile != NULL) {
+   fprintf(logFile,"%s\n",msg);
+   fflush(logFile);
+  }
+  V(&mutex);
+}
+
+/*
+ * logClose - close the log file
+ */
+void logClose() {
+  if(logFile != NULL)
+    fclose(logFile);
+}
+
+void sighupHandler(int sig){
+    hupReceived = 1;
+}
+
+void process(struct node *pointer){
+  struct sigaction action, old_action; 
+  DIR* watchDIR;
+  struct dirent* entp;
+  time_t timer;
+  char path[1024];
+  struct stat s;
+
+  char * pathName = strtok(pointer->text," ");
+  char * folderName = strtok(pointer->text," ");
+
+  /* Open log file */
+    if(logOpen() != 0) {
+        fprintf(stderr, "Could not open log file\n");
+        exit(1);
+    }
+
+    /* Switch to the background */
+  if(becomeDaemon(pathName) != 0) {
+      logMessage("Failed becomeDaemon\n");
+	    exit(1);
+    }
+
+  /* Install the handler for SIGTERM */
+  action.sa_handler = handleTERM;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+  sigaction(SIGTERM, &action, &old_action);
+
+  /* Run the initial scan. */
+  watchDIR = opendir(folderName);
+  while((entp = readdir(watchDIR))!=NULL) {
+    strcpy(path,folderName);
+	strcat(path,"/");
+	strcat(path,entp->d_name);
+	stat(path,&s);
+	/* Only pay attention to files, and ignore directories. */
+	if(S_ISREG(s.st_mode)) 
+	  check_file(folderName,entp->d_name,s.st_mtime,s.st_size);
+  }
+  closedir(watchDIR);
+
+  /* Start watching for changes. **/
+  run = 1;
+  while(run == 1) {
+    /* Note the time and go to sleep. */
+    time(&timer);
+    sleep(60);
+    /* Scan the watched directory for changes. */
+    watchDIR = opendir(folderName);
+    while((entp = readdir(watchDIR))!=NULL) {
+      strcpy(path,folderName);
+      strcat(path,"/");
+      strcat(path,entp->d_name);
+      stat(path,&s);
+      /* Only pay attention to files, and ignore directories. */
+      if(S_ISREG(s.st_mode)) {
+        /* Upload recently modified files. */
+        if(difftime(s.st_mtime,timer) > 0) {
+          upload_file(folderName,entp->d_name,s.st_size);
+        }
+      }
+    }
+    closedir(watchDIR);
+  }
+  return NULL;
+
+}
+
+void *thread(void *vargp){
+  Pthread_detach(pthread_self());
+  Free(vargp); 
+  process(currentNode);
+  return NULL;
+}
+
+int main(int argc, char **argv){ 
+  char myPort[MAXLINE], myPath[MAXLINE], myAddress[MAXLINE];
+  struct sigaction sa;
+  struct node *pointer;
+
+/** Install SIGHUP handler **/
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = sighupHandler;
+	if (sigaction(SIGHUP, &sa, NULL) == -1) {
+      fprintf(stderr, "Failed to install SIGHUP handler\n");
+      exit(1);
+  }
+
+    // /* Read configuration information */
+  if(readConfig(myAddress, myPort, pointer) != 0) {
+    fprintf(stderr, "Failed to read config file\n");
+    exit(1);
+  }
+
+  static pthread_once_t once = PTHREAD_ONCE_INIT;
+  Pthread_once(&once, init_echo_cnt);
+
+  currentNode = pointer;
+
+  while(currentNode->next != NULL) {
+    pthread_t tid;
+    Pthread_create(&tid, NULL, thread, NULL);
+    Pthread_join(tid, NULL);
+    exit(0);
+    currentNode = currentNode->next;
+  }
+    
+}
---
+++ threads/server/bu.c
@@ -0,0 +1,202 @@
+#include "csapp.h"
+
+/* Locations of important files */
+const char* pidFileLoc = "/run/bu.pid";
+const char* confFileLoc = "/etc/bu.conf";
+const char* logFileLoc = "/var/log/bu.log";
+FILE *logFile;
+
+static sem_t mutex;
+/* Global SIGHUP received flag */
+static volatile sig_atomic_t hupReceived = 0;
+
+void sighupHandler(int sig){
+    hupReceived = 1;
+}
+
+void serve_request(int connfd){
+    char buffer[MAXLINE], fileName[MAXLINE], filePath[MAXLINE];
+    char *fileBuf;
+    int size;
+    rio_t rio;
+    int fd;
+    struct stat s;
+
+    strcpy(filePath, "/var/bu/");
+    /* Read request */
+    Rio_readinitb(&rio, connfd);
+    if (!Rio_readlineb(&rio, buffer, MAXLINE))
+        return;
+    if (strcmp(buffer, "upload\n") == 0) {
+        Rio_readlineb(&rio, fileName, MAXLINE);
+        Rio_readlineb(&rio, buffer, MAXLINE);
+        printf("Request to upload %s\n",fileName);
+        strcat(filePath, fileName);
+        printf("FilePath: %s\n",filePath);
+        sscanf(buffer,"%d",&size);
+        fileBuf = Malloc(size);
+        if(fileBuf != NULL) {
+           Rio_readnb(&rio,fileBuf,size);
+           fd = Open(filePath,O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
+           Write(fd,fileBuf,size);
+           Close(fd);
+           Free(fileBuf);
+        }
+        strcpy(buffer,"stored\n");
+        Rio_writen(connfd,buffer,strlen(buffer));
+    } else if(strcmp(buffer,"status\n") == 0) {
+        Rio_readlineb(&rio, fileName, MAXLINE);
+        printf("Request to stat %s\n",fileName);
+        strcat(filePath, fileName);
+        printf("FilePath: %s\n",filePath);
+        filePath[strlen(filePath)-1] = '\0';
+        if(stat(filePath,&s) == 0)
+          sprintf(buffer,"%d\n",s.st_mtime);
+        else
+          strcpy(buffer,"0\n");
+        Rio_writen(connfd,buffer,strlen(buffer));
+    }
+}
+
+/*
+ * becomeDaemon - convert this process to a daemon
+ */
+int becomeDaemon(char *home){
+  int fd;
+  pid_t pid;
+  FILE* pidFile;
+
+  if((pid = Fork()) != 0) { /* Become background process */
+    exit(0);  /* Original parent terminates */
+  }
+
+  if(setsid() == -1) /* Become leader of new session */
+    return -1;
+
+  if((pid = Fork()) != 0) { /* Ensure we are not session leader */
+	/** Prepare pid file and terminate **/
+	pidFile = fopen(pidFileLoc,"w");
+	fprintf(pidFile,"%d",pid);
+	fclose(pidFile);
+    exit(0);
+  }
+
+  chdir(home); /* Change to home directory */
+
+  Close(STDIN_FILENO); /* Reopen standard fd's to /dev/null */
+
+  fd = Open("/dev/null", O_RDWR, 0);
+
+  if (fd != STDIN_FILENO)         /* 'fd' should be 0 */
+    return -1;
+  if (Dup2(STDIN_FILENO, STDOUT_FILENO) != STDOUT_FILENO)
+    return -1;
+  if (Dup2(STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO)
+    return -1;
+
+  return 0;
+}
+
+/*
+ * readConfig - read the configuration file
+ */
+int readConfig(char *port, char *path) {
+  FILE *config;
+
+  config = fopen(confFileLoc,"r");
+  if(config == NULL)
+    return -1;
+  fscanf(config,"%s",port);
+  fscanf(config,"%s",path);
+  fclose(config);
+  return 0;
+}
+
+/*
+ * logOpen - open the log file
+ */
+int logOpen() {
+  logFile = fopen(logFileLoc,"a");
+  if(logFile == NULL)
+    return -1;
+  return 0;
+}
+
+/* 
+ * logMessage - write a message to the log file
+ */
+void logMessage(char *msg) {
+  P(&mutex);
+  if(logFile != NULL) {
+   fprintf(logFile,"%s\n",msg);
+   fflush(logFile);
+  }
+  V(&mutex);
+}
+
+/*
+ * logClose - close the log file
+ */
+void logClose() {
+  if(logFile != NULL)
+    fclose(logFile);
+}
+
+
+/* thread routine */
+void *thread(void *vargp) {
+    int connfd = *((int *)vargp);
+    Pthread_detach(pthread_self()); //line:conc:echoservert:detach
+    Free(vargp);                    //line:conc:echoservert:free
+    serve_request(connfd);
+    Close(connfd);
+    return NULL;
+}
+
+int main(int argc, char **argv){
+    int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
+    char myPort[MAXLINE], myHome[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    struct sigaction sa;
+    pthread_t tid; 
+
+    /** Install SIGHUP handler **/
+	  sigemptyset(&sa.sa_mask);
+	  sa.sa_flags = SA_RESTART;
+	  sa.sa_handler = sighupHandler;
+	  if (sigaction(SIGHUP, &sa, NULL) == -1) {
+        fprintf(stderr, "Failed to install SIGHUP handler\n");
+        exit(1);
+      }
+
+    /* Read configuration information */
+    if(readConfig(myPort,myHome) != 0) {
+        fprintf(stderr, "Failed to read config file\n");
+        exit(1);
+    }
+
+    /* Open log file */
+    if(logOpen() != 0) {
+        fprintf(stderr, "Could not open log file\n");
+        exit(1);
+    }
+
+    /* Switch to the background */
+	if(becomeDaemon(myHome) != 0) {
+        logMessage( "Failed becomeDaemon\n");
+	    exit(1);
+    }
+
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
+        Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE,
+                    port, MAXLINE, 0);
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
+        
+        Pthread_create(&tid, NULL, thread, connfd);
+    }
+}
